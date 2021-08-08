463cd4a65c2c64b76dd3eec8a02d80f1ab630fc5
nissrine saraireh <nissrine.saraireh@gmail.com>
2021-08-08 11:57:31 +0200

    updated the icon display tag

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
+let city = "jordan";
 let apiKey = "ab10edc1d32f1dd18832060f89f088c3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 let today = new Date();
-
-function formatDate(timestamp) {
-  console.log(timestamp);
-  let today = new Date();
-  let hour = timestamp.getHours;
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let day = days[timestamp.getDay()];
-  let minute = timestamp.getMinutes().toString().padStart(2, "0");
-  let time = `${hour}:${minute}`;
-  let showDate = `${day} ${time}`;
-  return showDate;
-
-  //   let showDateElement = document.querySelector("#show-date-time");
-  //   showDateElement.innerHTML = formatDate(dateElement);
-}
 
 function displayTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
@@ -32,22 +9,30 @@ function displayTemperature(response) {
   let weatherdescription = response.data.weather[0].main;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
-
-  let dateElement = response.data.dt * 10000;
-
+  let iconImage = response.data.weather[0].icon;
+  let iconLink = `https://openweathermap.org/img/wn/${iconImage}.png`;
+  let altText = response.data.weather[0].description;
+  // let dateElement = response.data.dt;
+  console.log(response.data.dt);
   let showCity = document.querySelector("#city");
   let showWeatherDescription = document.querySelector("#weather-description");
   let showTemperature = document.querySelector("#temperature-display");
   let showHumidity = document.querySelector("#humidity");
   let showWind = document.querySelector("#wind");
+  let showIcon = document.querySelector("#weather-icon");
 
   showTemperature.innerHTML = temperature;
   showWeatherDescription.innerHTML = weatherdescription;
   showCity.innerHTML = searchedForCity;
   showHumidity.innerHTML = humidity;
   showWind.innerHTML = wind;
+  showIcon.setAttribute("src", iconLink);
+  showIcon.setAttribute("alt", altText);
+
   showDate(today);
-  formatDate(dateElement);
+  // formatDate(dateElement);
+
+  console.log(response.data.weather[0].icon);
 }
 
 function showDate(today) {
