60e5511626e4adbbf8880720320d40496e4f340f
nissrine saraireh <nissrine.saraireh@gmail.com>
2021-08-10 16:18:00 +0200

    started js to dynamically display weather forecast html block

---
+++ src/forecast-display.js
@@ -0,0 +1,20 @@
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerhtml = "forcast";
+  //   <div class="row">
+  /* <!-- JS manipulated column -->
+              <div class="col-2 forecast-first-day">
+                <div class="weather-forecast-date">Wednesday</div>
+                <img
+                  src="https://openweathermap.org/img/wn/01d.png"
+                  alt="forecast-icon"
+                  class="weather-forecast-icon"
+                  />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max"> 18°</span> |
+                  <span class="weather-forecast-temperature-min"> 12°</span>
+                </div>
+              </div>
+            </div>
+          </div> */
+}
